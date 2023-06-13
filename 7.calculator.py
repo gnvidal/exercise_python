e89210aeeb114f42dce4f0dac54994db96f35414
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-06-12 22:33:39 -0400

    Create 7.calculator.py

---
+++ 7.calculator.py
@@ -0,0 +1,13 @@
+def calculato(n1,n2):
+    n1=float(input("enter your first number: "))
+    n2=float(input("enter your second number: "))
+    option=0
+    while True:print("""
+    Tell me, What you want to do??
+    1) Sum
+    2) Subtract
+    3) Multiply
+    4) Divide
+    
+    
+    """)
